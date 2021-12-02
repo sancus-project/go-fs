e140f3f69570775bd1b193b3e4bd1886548d2cd5
Alejandro Mery <amery@jpi.io>
2021-12-02 13:13:15 +0000

    fuse: implement Filesystem.Close()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- fuse/filesystem.go
+++ fuse/filesystem.go
@@ -2,6 +2,7 @@ package fuse
 
 import (
 	"io/fs"
+	"log"
 
 	"bazil.org/fuse"
 )
@@ -10,6 +11,24 @@ type Filesystem struct {
 	store fs.FS
 	conn  *fuse.Conn
 	dir   string
+}
+
+func (fsys *Filesystem) Close() error {
+	// Attempt to close the fuse connection
+	if err := fsys.conn.Close(); err != nil {
+		return err
+	}
+
+	if err := fsys.Unmount(); err != nil {
+		log.Println(err)
+	}
+
+	// Close store if a Close() is implemented
+	if m, ok := fsys.store.(interface{ Close() error }); ok {
+		return m.Close()
+	}
+
+	return nil
 }
 
 func New(store fs.FS, dir string, options ...fuse.MountOption) (*Filesystem, error) {
