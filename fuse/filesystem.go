3db92dd39f9cd027aa79da5eacc8266466a3b84d
Alejandro Mery <amery@jpi.io>
2021-12-02 13:11:09 +0000

    fuse: rename private `fs` to `store` to reduce confusion
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- fuse/filesystem.go
+++ fuse/filesystem.go
@@ -7,21 +7,21 @@ import (
 )
 
 type Filesystem struct {
-	fs   fs.FS
-	conn *fuse.Conn
-	dir  string
+	store fs.FS
+	conn  *fuse.Conn
+	dir   string
 }
 
-func New(fsys fs.FS, dir string, options ...fuse.MountOption) (*Filesystem, error) {
+func New(store fs.FS, dir string, options ...fuse.MountOption) (*Filesystem, error) {
 	conn, err := fuse.Mount(dir, options...)
 	if err != nil {
 		return nil, err
 	}
 
 	m := &Filesystem{
-		fs:   fsys,
-		conn: conn,
-		dir:  dir,
+		store: store,
+		conn:  conn,
+		dir:   dir,
 	}
 
 	return m, nil
