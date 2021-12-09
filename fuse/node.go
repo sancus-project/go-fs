37fc02a995d0c49a05d375915b24e640c249ac36
Alejandro Mery <amery@jpi.io>
2021-12-09 23:33:09 +0000

    fuse: don't keep an open handler inside Node{}
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- fuse/node.go
+++ fuse/node.go
@@ -3,16 +3,18 @@ package fuse
 import (
 	"context"
 	"fmt"
-	"runtime"
 
 	"go.sancus.dev/fs"
 	"go.sancus.dev/fs/fuse/types"
 )
 
+var (
+	_ types.Node = (*Node)(nil)
+)
+
 type Node struct {
 	name string
 	fs   *Filesystem
-	f    fs.File
 }
 
 func (node *Node) String() string {
@@ -22,7 +24,7 @@ func (node *Node) String() string {
 func (node *Node) Attr(ctx context.Context, attr *types.Attr) error {
 
 	// fs.FileInfo
-	fi, err := node.f.Stat()
+	fi, err := fs.Stat(node.fs.store, node.name)
 	if err != nil {
 		return nil
 	}
@@ -47,18 +49,14 @@ func (fsys *Filesystem) open(name string) (types.Node, error) {
 
 	if name == "." && fsys.root != nil {
 		return fsys.root, nil
-	} else if f, err := fsys.store.Open(name); err != nil {
+	} else if _, err := fs.Stat(fsys.store, name); err != nil {
 		return nil, err
 	} else {
 		node := &Node{
 			name: name,
 			fs:   fsys,
-			f:    f,
 		}
 
-		runtime.SetFinalizer(node, func(node *Node) {
-			node.f.Close()
-		})
 		return node, nil
 	}
 }
@@ -67,26 +65,16 @@ func (fsys *Filesystem) opendir(name string) (types.Node, error) {
 
 	if name == "." && fsys.root != nil {
 		return fsys.root, nil
-	} else if f, err := fsys.store.Open(name); err != nil {
-		return nil, err
-	} else if fi, err := f.Stat(); err != nil {
-		f.Close()
+	} else if fi, err := fs.Stat(fsys.store, name); err != nil {
 		return nil, err
 	} else if !fi.IsDir() {
 		err = &fs.PathError{"opendir", name, types.ENOTDIR}
-
-		f.Close()
 		return nil, err
 	} else {
 		node := &Node{
 			name: name,
 			fs:   fsys,
-			f:    f,
 		}
-
-		runtime.SetFinalizer(node, func(node *Node) {
-			node.f.Close()
-		})
 		return node, nil
 	}
 }
