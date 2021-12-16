b7d24cf491c9127176b7f327bf59ab81385f80da
Alejandro Mery <amery@jpi.io>
2021-12-16 01:38:33 +0000

    fuse: remove redundant Getattr()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- fuse/node_attr.go
+++ fuse/node_attr.go
@@ -3,14 +3,12 @@ package fuse
 import (
 	"context"
 
-	"bazil.org/fuse"
 	"go.sancus.dev/fs"
 	"go.sancus.dev/fs/fuse/types"
 )
 
 var (
-	_ types.Node          = (*Node)(nil)
-	_ types.NodeGetattrer = (*Node)(nil)
+	_ types.Node = (*Node)(nil)
 )
 
 func (node *Node) Attr(ctx context.Context, attr *types.Attr) error {
@@ -35,8 +33,4 @@ func (node *Node) Attr(ctx context.Context, attr *types.Attr) error {
 	}
 
 	return nil
-}
-
-func (node *Node) Getattr(ctx context.Context, req *fuse.GetattrRequest, resp *fuse.GetattrResponse) error {
-	return node.Attr(ctx, &resp.Attr)
 }
