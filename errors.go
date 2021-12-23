a6c7ccb1ef0c9528ff56f5e404fc44857b166fab
Alejandro Mery <amery@jpi.io>
2021-12-23 15:45:44 +0000

    Add AsSyscallPathError() and improve AsPathError()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- errors.go
+++ errors.go
@@ -1,12 +1,39 @@
 package fs
 
+import (
+	"io/fs"
+	"os"
+	"syscall"
+)
+
+// AsSyscallPathError() returns a fs.PathError indicating a syscall.Errno.
+// Returns nil if err == 0 (aka success)
+func AsSyscallPathError(op string, path string, err syscall.Errno) error {
+	if err == 0 {
+		return nil
+	}
+
+	return &fs.PathError{op, path, err}
+}
+
+// AsPathError() wraps an error in fs.PathError, unwrapping known error
+// types, and returning nil if nil is given.
 func AsPathError(op string, path string, err error) error {
 	if err == nil {
 		return nil
-	} else if pe, ok := err.(*PathError); ok {
-		// reuse
-		return pe
-	} else {
-		return &PathError{op, path, err}
 	}
+
+	switch e2 := err.(type) {
+	case syscall.Errno:
+		return AsSyscallPathError(op, path, e2)
+	case *fs.PathError:
+		// reuse op and err, not path
+		op, err = e2.Op, e2.Err
+	case *os.LinkError:
+		err = e2.Err
+	case *os.SyscallError:
+		err = e2.Err
+	}
+
+	return &fs.PathError{op, path, err}
 }
