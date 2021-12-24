c80533999dc391b715e3feb36540783bb46d83a3
Alejandro Mery <amery@jpi.io>
2021-12-24 03:46:53 +0000

    Improve Create and WriteFile handling of ENOSYS and add documentation
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- create.go
+++ create.go
@@ -8,36 +8,63 @@ import (
 	"syscall"
 )
 
+// Create creates or truncates the named file from the file system.
+// If the file already exists, it is truncated.
+// If the file does not exist, it is created with mode 0666.
+// If successful, methods on the returned File can be used for I/O; the associated file
+// descriptor has mode O_RDWR. If there is an error, it will be of type *PathError.
+//
+// If fs implements CreateFS, Create calls fsys.Create. Otherwise it tries
+// fsys.OpenFile
 func Create(fsys fs.FS, name string) (File, error) {
 	if fsys, ok := fsys.(CreateFS); ok {
-		return fsys.Create(name)
+		f, err := fsys.Create(name)
+		if !IsNotImplemented(err) {
+			return f, err
+		}
 	}
 
 	if fsys, ok := fsys.(OpenFileFS); ok {
-		return fsys.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+		return fsys.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0666)
 	}
 
-	err := &PathError{"create", name, syscall.ENOSYS}
+	err := AsSyscallPathError("create", name, syscall.ENOSYS)
 	return nil, err
 }
 
+// create2 is a variant of Create trying OpenFile first to pass the permissions.
+// when fsys.OpenFile is used instead of fsys.Create the associated file
+// descriptor has mode O_WRONLY.
 func create2(fsys fs.FS, name string, perm fs.FileMode) (File, error) {
 	if fsys, ok := fsys.(OpenFileFS); ok {
-		return fsys.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, perm)
+		f, err := fsys.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, perm)
+		if !IsNotImplemented(err) {
+			return f, err
+		}
 	}
 
 	if fsys, ok := fsys.(CreateFS); ok {
 		return fsys.Create(name)
 	}
 
-	err := &PathError{"create", name, syscall.ENOSYS}
+	err := AsSyscallPathError("create", name, syscall.ENOSYS)
 	return nil, err
 }
 
+// WriteFile writes data to the named file from the file system, creating it if necessary.
+// If the file does not exist, WriteFile creates it with permissions perm; otherwise WriteFile
+// truncates it before writing, without changing permissions.
+// If there is an error, it will be of type *PathError
+//
+// If fs implements WriteFileFS, WriteFile calls fsys.WriteFile. Otherwise it uses fsys.OpenFile or fsys.Create,
+// Writes the data, and closes the file.
 func WriteFile(fsys fs.FS, name string, data []byte, perm fs.FileMode) error {
 
 	if fsys, ok := fsys.(WriteFileFS); ok {
-		return fsys.WriteFile(name, data, perm)
+		err := fsys.WriteFile(name, data, perm)
+		if !IsNotImplemented(err) {
+			return err
+		}
 	}
 
 	f, err := create2(fsys, name, perm)
@@ -45,7 +72,7 @@ func WriteFile(fsys fs.FS, name string, data []byte, perm fs.FileMode) error {
 		if f, ok := f.(io.Writer); ok {
 			_, err = f.Write(data)
 		} else {
-			err = &PathError{"write", name, syscall.EPERM}
+			err = AsSyscallPathError("write", name, syscall.EPERM)
 		}
 
 		if err1 := f.Close(); err1 != nil && err == nil {
